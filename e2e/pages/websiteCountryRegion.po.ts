969ffafa26b51c804d3989400e3933db6cb4c78a
Fodale <Angela.M.Fodale@questdiagnostics.com>
2020-08-06 13:27:48 -0400

    Website Country/Region page object

---
+++ e2e/pages/websiteCountryRegion.po.ts
@@ -0,0 +1,7 @@
+import * as globalMethods from 'questglobal';
+import { element, by, browser } from 'protractor';
+
+
+var projectGlobal = require('../global/global');
+export class WebsiteCountryRegion{
+}
